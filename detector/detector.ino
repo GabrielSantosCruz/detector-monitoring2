d237a58750822f46067ebefb245bafddc5b4611b
Gabriel Santos <gabrielsantoscruz61@gmail.com>
2025-07-28 10:52:00 -0300

    ✨ feat(geiger detector setups): added configurations and pinmodes

--- detector/detector.ino
+++ detector/detector.ino
@@ -8,27 +8,28 @@ PCF8563_Class rtc;
 #define RTC_INT_PIN 10  // GPIO conectada ao INT do PCF8563
 #define RTC_SDA_PIN 8
 #define RTC_SCL_PIN 9
+#define GEIGER_PIN  0
 
 volatile bool alarmeDisparado = false;
+int pulsosDesdeUltimoWakeup;
 
 void IRAM_ATTR handleRtcInterrupt();
 void clearRtcAlarmFlag();
+void IRAM_ATTR contarPulsoISR();
 
 void setup() {
   Serial.begin(115200);
-  delay(2000);  // Dá tempo de abrir o monitor serial
+  delay(2000);  
 
-  Wire.begin(RTC_SDA_PIN, RTC_SCL_PIN);  // Ajuste para os seus pinos SDA e SCL
+  Wire.begin(RTC_SDA_PIN, RTC_SCL_PIN);  
 
   rtc.begin(Wire);
 
-  // Configura data/hora inicial (ajuste para hora atual)
   rtc.setDateTime(2025, 7, 25, 10, 0, 0);
 
   // Limpa alarmes anteriores e flags
   rtc.disableAlarm();
 
-  // Programa alarme para 1 minuto após o minuto atual
   RTC_Date now = rtc.getDateTime();
   uint8_t alarmMinute = (now.minute + 1) % 60;
   rtc.setAlarmByMinutes(alarmMinute);
@@ -41,6 +42,11 @@ void setup() {
 
   Serial.printf("Alarme programado para %02d:%02d\n", now.hour, alarmMinute);
   Serial.println("Aguardando alarme...");
+
+  pinMode(GEIGER_PIN, INPUT);  // Configura o pino como entrada
+
+  // Configura interrupção na borda de descida
+  attachInterrupt(digitalPinToInterrupt(GEIGER_PIN), contarPulso, FALLING);
 }
 
 void IRAM_ATTR handleRtcInterrupt() {
@@ -68,6 +74,9 @@ void clearRtcAlarmFlag() {
   Wire.endTransmission();
 }
 
+void IRAM_ATTR contarPulsoISR() {
+  pulsosDesdeUltimoWakeup++;
+}
 
 void loop() {
   RTC_Date now = rtc.getDateTime();
